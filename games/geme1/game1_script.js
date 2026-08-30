31f8a16661a01d62504b2e6a14f4a18491725af7
wakb <chipstaranpan@gmail.com>
2026-08-30 09:41:59 +0900

    編集

--- games/geme1/game1_script.js
+++ games/geme1/game1_script.js
@@ -1,6 +1,29 @@
-const button = document.getElementById('start');
-const text = document.getElementById('myText');
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
 
-button.addEventListener('click', function () {
-    text.textContent = 'テキスト';
+    function drawbar(position, length) {
+        ctx.fillStyle = "#00ffcc";
+        ctx.beginPath();
+
+        if (length >= 40) {
+            ctx.moveTo(position, 400);
+            ctx.lineTo(position, 480);
+            ctx.lineTo(position + length, 480);
+            ctx.lineTo(position + length, 440);
+            ctx.lineTo(position + 40, 440);
+        }
+
+        if (length < 40) {
+            ctx.moveTo(position, 400);
+            ctx.lineTo(position, 480);
+            ctx.lineTo(position + length, 480);
+            ctx.lineTo(position + length, 400 + length);
+        }
+
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    drawbar(100, 60)
 });
