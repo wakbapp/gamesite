e4c902ff42bcfac0c136c42e5646985a24692bc4
wakb <chipstaranpan@gmail.com>
2026-08-30 10:38:40 +0900

    編集

--- games/geme1/game1_script.js
+++ games/geme1/game1_script.js
@@ -2,34 +2,34 @@ window.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
-    function drawbar(position, length) {
+    function drawbar(position, beat) {
+        length = beat * 48
         ctx.fillStyle = "#00ffcc";
         ctx.beginPath();
 
-        if (length >= 40) {
+        if (length > 48) {
             ctx.moveTo(position, 200);
-            ctx.lineTo(position, 280);
-            ctx.lineTo(position + length - 5, 280);
-            ctx.lineTo(position + length - 5, 240);
-            ctx.lineTo(position + 40, 240);
+            ctx.lineTo(position, 288);
+            ctx.lineTo(position + length - 4, 288);
+            ctx.lineTo(position + length - 4, 244);
+            ctx.lineTo(position + 44, 244);
         }
 
-        if (length < 40) {
+        if (length <= 48) {
             ctx.moveTo(position, 200);
-            ctx.lineTo(position, 280);
-            ctx.lineTo(position + length, 280);
-            ctx.lineTo(position + length, 200 + length);
+            ctx.lineTo(position, 288);
+            ctx.lineTo(position + length - 4, 288);
+            ctx.lineTo(position + length - 4, 196 + length);
         }
 
         ctx.closePath();
         ctx.fill();
     }
 
-    drawbar(0, 160);
-    drawbar(160, 160);
-    drawbar(320, 160);
-    drawbar(480, 160);
-    drawbar(640, 160);
-    drawbar(800, 160);
+    drawbar(0, 1);
+    drawbar(48, 1);
+    drawbar(96, 1);
+    drawbar(576, 4);
+    drawbar(768, 4);
 
 });
